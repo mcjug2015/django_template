b3924f57f8011b984beab64c759d13dadf97c26b
mcjug2015 <victor.semenov@gmail.com>
2015-07-25 12:40:59 -0400

    ignoring central settings, some fab and db work

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,10 @@ env.prj_name = 'django_template'
 env.path = '/opt/%(prj_name)s' % env
 
 
+def local_box():
+    env.instance = 'local'
+
+
 def _ensure_virtualenv():
     if "VIRTUAL_ENV" not in os.environ:
         sys.stderr.write("$VIRTUAL_ENV not found. Make sure to activate virtualenv first\n\n")
@@ -16,7 +20,11 @@ def _ensure_virtualenv():
     env.virtualenv = os.environ["VIRTUAL_ENV"]
 
 
+def copy_settings():
+    require('instance')
+    local('cp django_template/environments/%(instance)s/settings/%(instance)s.py django_template/settings.py' % env)
+
+
 def install_prod_deps():
     _ensure_virtualenv()
     local('pip install -q -r %(path)s/dependencies/prod.txt' % env)
-
