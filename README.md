62ce60c44e1d7ea00390617bc183665c8697e91f
mcjug2015 <victor.semenov@gmail.com>
2015-10-17 12:27:00 -0400

    adding lat, long, and distance filtering to cigar shops

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # django_template
 A template django project
+
+To query cigar shops by lat, long, and distance in miles do:
+http://127.0.0.1:8000/api/v1/cigarshop/?lat=37.067922&long=-75.130205&distance=10&format=json
--- template_app/api.py
+++ template_app/api.py
@@ -3,17 +3,43 @@ from tastypie.contrib.gis.resources import ModelResource
 from template_app.models import CigarShop, FaveShops
 from tastypie.authentication import SessionAuthentication
 from tastypie.authorization import DjangoAuthorization
+from tastypie.constants import ALL
+from django.contrib.gis.geos.factory import fromstr
+from django.db.models import Q
+from django.contrib.gis.measure import D
 
 
 class CigarShopResource(ModelResource):
     ''' tastypie resource for cigarshop '''
+
+    def build_filters(self, filters=None):
+        if filters is None:
+            filters = {}
+        orm_filters = super(CigarShopResource, self).build_filters(filters)
     
+        if('lat' in filters and 'long' in filters and 'distance' in filters):
+            pnt = fromstr('POINT(%s %s)' % (filters['long'], filters['lat']), srid=4326)
+            orm_filters.update({'custom': Q(location__distance_lte=(pnt, D(mi=filters['distance'])))})
+    
+        return orm_filters
+
+    def apply_filters(self, request, applicable_filters):
+        if 'custom' in applicable_filters:
+            custom = applicable_filters.pop('custom')
+        else:
+            custom = None
+    
+        semi_filtered = super(CigarShopResource, self).apply_filters(request, applicable_filters)
+    
+        return semi_filtered.filter(custom) if custom else semi_filtered
+
     class Meta(object):
         ''' meta info '''
         queryset = CigarShop.objects.all()
         allowed_methods = ['get']
         authentication = SessionAuthentication()
         authorization = DjangoAuthorization()
+        filtering = {'location': ALL}
 
 
 class FaveShopsResource(ModelResource):
