e26f0eff60baaa821542c4333a7d1c27b7cf944d
mcjug2015 <victor.semenov@gmail.com>
2016-01-30 13:09:14 -0500

    adding more integration tests

--- template_app/tests/py_integration/api_tests.py
+++ template_app/tests/py_integration/api_tests.py
@@ -2,7 +2,7 @@
 # pylint: disable=E1101,  W0212
 import json
 from django.test.testcases import TestCase
-from template_app.models import CigarShop
+from template_app.models import CigarShop, FaveShops
 
 
 class UserResourceTests(TestCase):
@@ -92,3 +92,109 @@ class CigarShopResourceTests(TestCase):
         response = self.client.get(new_obj_uri)
         self.assertEquals(response.status_code, 404)
         self.assertEquals(CigarShop.objects.filter(owner__pk=1).count(), 2)
+
+    def test_crud_unauthorized(self):
+        '''
+            verify that other users attempts to alter a users cigar shops are rejected
+        '''
+        self.assertEquals(CigarShop.objects.filter(owner__pk=1).count(), 2)
+        self.client.login(username='another_test_user', password='testing123')
+        cigar_shop_obj = {'name': 'brand new cigar shop',
+                          'owner': '/api/v1/auth/user/1/',
+                          "location": {"coordinates": [-77.0, 39.0], "type": "Point"}}
+        response = self.client.post('/api/v1/cigarshop/',
+                                    json.dumps(cigar_shop_obj),
+                                    content_type='application/json')
+        self.assertEquals(response.status_code, 401)
+
+        response = self.client.get('/api/v1/cigarshop/1/')
+        self.assertEquals(response.status_code, 401)
+
+        cigar_shop_obj['name'] = 'the name has been changed'
+        # put to update, must include entire obj as all fields get updated
+        response = self.client.put('/api/v1/cigarshop/1/',
+                                   json.dumps(cigar_shop_obj),
+                                   content_type='application/json')
+        self.assertEquals(response.status_code, 401)
+
+        response = self.client.patch('/api/v1/cigarshop/1/',
+                                     json.dumps({'name': 'no location no problem'}),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, 401)
+
+        response = self.client.delete('/api/v1/cigarshop/1/')
+        self.assertEquals(response.status_code, 401)
+
+    def test_lat_long_filter(self):
+        ''' make sure requests that filter by lat, long and distance work '''
+        self.client.login(username='test_user', password='testing123')
+        # request that filters narrows results down
+        response = self.client.get('/api/v1/cigarshop/?long=-77.0&lat=39.0&distance=0.01')
+        self.assertEquals(response.status_code, 200)
+        json_obj = json.loads(response.content)
+        self.assertEquals(1, len(json_obj['objects']))
+
+        # request the does not gets all results. all 3 params must be present to filter.
+        response = self.client.get('/api/v1/cigarshop/?&lat=39.0&distance=0.01')
+        self.assertEquals(response.status_code, 200)
+        json_obj = json.loads(response.content)
+        self.assertEquals(2, len(json_obj['objects']))
+
+
+class FaveShopsResourceTests(TestCase):
+    ''' integration-ish tests for FaveShopsResource '''
+    fixtures = ['users_groups_perms.json', 'cigarshops_favs.json']
+
+    def test_crud_success(self):
+        '''
+            make sure authenticated user able to perform fave shops crud operations
+            on fave shops he has access to.
+        '''
+        self.assertEquals(FaveShops.objects.filter(owner__pk=1).count(), 1)
+        fave_shop_obj = {'name': 'another faves list',
+                         'owner': '/api/v1/auth/user/1/',
+                         'cigar_shops': []}
+        self.client.login(username='test_user', password='testing123')
+        # post to create a new fave shops list
+        response = self.client.post('/api/v1/faveshops/',
+                                    json.dumps(fave_shop_obj),
+                                    content_type='application/json')
+        self.assertEquals(response.status_code, 201)
+        self.assertEquals(FaveShops.objects.filter(owner__pk=1).count(), 2)
+        new_obj_uri = response._headers['location'][1]
+
+        # get the newly created cigarshop
+        response = self.client.get(new_obj_uri)
+        self.assertEquals(response.status_code, 200)
+        retrieved_new_obj = json.loads(response.content)
+        self.assertEquals(retrieved_new_obj['name'], fave_shop_obj['name'])
+
+        fave_shop_obj['cigar_shops'].append('/api/v1/cigarshop/1/')
+        # put to update, must include entire obj as all fields get updated
+        response = self.client.put(new_obj_uri,
+                                   json.dumps(fave_shop_obj),
+                                   content_type='application/json')
+        self.assertEquals(response.status_code, 204)
+
+        response = self.client.get(new_obj_uri)
+        self.assertEquals(response.status_code, 200)
+        retrieved_new_obj = json.loads(response.content)
+        self.assertIn('/api/v1/cigarshop/1/', retrieved_new_obj['cigar_shops'])
+
+        # patch to only update a single field
+        response = self.client.patch(new_obj_uri,
+                                     json.dumps({'cigar_shops': ['/api/v1/cigarshop/2/']}),
+                                     content_type='application/json')
+        self.assertEquals(response.status_code, 202)
+
+        response = self.client.get(new_obj_uri)
+        self.assertEquals(response.status_code, 200)
+        retrieved_new_obj = json.loads(response.content)
+        self.assertIn('/api/v1/cigarshop/2/', retrieved_new_obj['cigar_shops'])
+
+        # delete to get rid of the object
+        response = self.client.delete(new_obj_uri)
+        self.assertEquals(response.status_code, 204)
+        response = self.client.get(new_obj_uri)
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(FaveShops.objects.filter(owner__pk=1).count(), 1)
