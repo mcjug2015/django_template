261120c5f5a5cd98a7583fed51c509f34eedb485
mcjug2015 <victor.semenov@gmail.com>
2016-02-16 22:37:20 -0500

    adding a directive for a list of cigarshops. tests messy, need refactoring

--- template_app/front_end_qc/tests/template_app/cigarshops/directives.js
+++ template_app/front_end_qc/tests/template_app/cigarshops/directives.js
@@ -1,21 +1,32 @@
 describe("tests for the cigar shops directives", function() {
     "use strict";
-    beforeEach(module("template_app"));
-    beforeEach(module('test_html_js'));
     var elm;
     var $rootScope;
     var $compile;
     var parentScope;
     var dirScope;
+    var $q;
+    var cigarShopSpy;
+    var returnedCigarShops;
+
+    beforeEach(module("template_app"));
+    beforeEach(module('test_html_js'));
+    beforeEach(module(function($provide) {
+        cigarShopSpy = jasmine.createSpyObj('cigarShopSpy', ['get']);
+        $provide.value("CigarShop", cigarShopSpy);
+    }));
+    
+    beforeEach(inject(function(_$rootScope_, _$compile_, _$q_) {
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+        $q = _$q_;
+        parentScope = $rootScope.$new();
+    }));
     
     describe("tests for the cigar shop directive", function() {
-        
-        beforeEach(inject(function(_$rootScope_, _$compile_) {
+        beforeEach(function() {
             elm = angular.element("<dt-cigar-shop shop='theShop'></dt-cigar-shop>");
-            $rootScope = _$rootScope_;
-            $compile = _$compile_;
-            parentScope = $rootScope.$new();
-        }));
+        });
         
         it("fills out the dtCigarShop template with shop info", function() {
             parentScope.theShop = {'name': 'Test shop for directive',
@@ -30,6 +41,33 @@ describe("tests for the cigar shops directives", function() {
             expect(dirScope.shop).toBeDefined();
             expect(dirScope.shop.name).toEqual('Test shop for directive');
         });
-
+    });
+    
+    describe("tests for the cigar shops directive", function() {
+        beforeEach(function() {
+            returnedCigarShops = [];
+            elm = angular.element("<dt-cigar-shops></dt-cigar-shops>");
+            cigarShopSpy.get.and.returnValue({'$promise': $q.when({'objects': returnedCigarShops})});
+        });
+        
+        it("fills out the dtCigarShops template when no cigar shops are returned", function() {
+            $compile(elm)(parentScope);
+            parentScope.$digest();
+            dirScope = elm.isolateScope();
+            expect(elm.find('span').text()).toEqual('Here are your cigar shops:');
+            expect(elm.find('dt-cigar-shop').size()).toEqual(0);
+        });
+        
+        it("fills out the dtCigarShops template when cigar shops are returned", function() {
+            returnedCigarShops.push({'id': 'a',
+                                     'name': 'Test shop for directive',
+                                     'owner': '/api/v1/auth/user/1/',
+                                     "location": {"coordinates": [-75.130205, 37.067922], "type": "Point"}});
+            $compile(elm)(parentScope);
+            parentScope.$digest();
+            dirScope = elm.isolateScope();
+            expect(elm.find('span').text()).toEqual('Here are your cigar shops:');
+            expect(elm.find('dt-cigar-shop').size()).toEqual(1);
+        });
     });
 });
--- template_app/static/js/template_app/cigarshops/directives.js
+++ template_app/static/js/template_app/cigarshops/directives.js
@@ -10,3 +10,22 @@ angular.module('template_app').directive('dtCigarShop', [function() {
         },
     };
 }]);
+
+
+angular.module('template_app').directive('dtCigarShops', ['CigarShop', function(CigarShop) {
+    "use strict";
+    return {
+        restrict: 'E',
+        templateUrl: '/static/html/template_app/cigarshops/cigar_shops.html',
+        scope: {
+        },
+        link: function (scope, element, attrs) {
+            scope.shops = {};
+            CigarShop.get().$promise.then(function(data) {
+                data.objects.forEach(function(curShop) {
+                    scope.shops[curShop.id] = curShop;
+                });
+            });
+        },
+    };
+}]);
