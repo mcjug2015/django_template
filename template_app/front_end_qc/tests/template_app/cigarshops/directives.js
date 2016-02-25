9fbb15440b33650f723e26f9153237ea17f014b9
mcjug2015 <victor.semenov@gmail.com>
2016-02-24 22:01:17 -0500

    adding ability to do full updates on cigarshops

--- template_app/front_end_qc/tests/template_app/cigarshops/directives.js
+++ template_app/front_end_qc/tests/template_app/cigarshops/directives.js
@@ -14,7 +14,7 @@ describe("tests for the cigar shops directives", function() {
     beforeEach(module("template_app"));
     beforeEach(module('test_html_js'));
     beforeEach(module(function($provide) {
-        cigarShopSpy = jasmine.createSpyObj('cigarShopSpy', ['get', 'remove']);
+        cigarShopSpy = jasmine.createSpyObj('cigarShopSpy', ['get', 'remove', 'update']);
         $provide.value("CigarShop", cigarShopSpy);
 
         saveAndGetSpy = jasmine.createSpy('saveAndGet');
@@ -30,38 +30,107 @@ describe("tests for the cigar shops directives", function() {
     
     describe("tests for the cigar shop directive", function() {
         beforeEach(function() {
-            elm = angular.element("<dt-cigar-shop shop='theShop' editable='modify'></dt-cigar-shop>");
+            elm = angular.element("<dt-cigar-shop shop='theShop'></dt-cigar-shop>");
+            cigarShopSpy.update.and.returnValue({'$promise': $q.when(true)});
         });
         
         it("fills out the dtCigarShop template with shop info", function() {
-            parentScope.modify = false;
             parentScope.theShop = {'name': 'Test shop for directive',
                                    'owner': '/api/v1/auth/user/1/',
-                                   "location": {"coordinates": [-75.130205, 37.067922], "type": "Point"}};
+                                   "location": {"coordinates": [-75.130205, 37.067922], "type": "Point"},
+                                   'beingEdited': false,
+                                   'editable': true};
             $compile(elm)(parentScope);
             parentScope.$digest();
             dirScope = elm.isolateScope();
             expect(elm.find('div div').first().text()).toEqual('Test shop for directive');
             expect(elm.find('div div div').first().text()).toEqual('37.067922');
             expect(elm.find('div div div').last().text()).toEqual('-75.130205');
+            expect(elm.find('div button').text()).toEqual('Update');
             expect(dirScope.shop).toBeDefined();
             expect(dirScope.shop.name).toEqual('Test shop for directive');
         });
         
         it("fills out the dtCigarShop template with editable shop info", function() {
-            parentScope.modify = true;
             parentScope.theShop = {'name': 'Test editable shop',
                                    'owner': '/api/v1/auth/user/1/',
-                                   "location": {"lat": 1.3, "long": 6.8}};
+                                   "location": {"lat": 1.3, "long": 6.8},
+                                   'beingEdited': true,
+                                   'editable': true};
             $compile(elm)(parentScope);
             parentScope.$digest();
             dirScope = elm.isolateScope();
             expect(dirScope.shop).toBeDefined();
             expect(dirScope.shop.name).toEqual('Test editable shop');
-            expect(dirScope.editable).toEqual(true);
             expect(elm.find('div input').first().val()).toEqual('Test editable shop');
             expect(elm.find('div div input').first().val()).toEqual('1.3');
             expect(elm.find('div div input').last().val()).toEqual('6.8');
+            expect(elm.find('div button').first().text()).toEqual('Save Update');
+            expect(elm.find('div button').last().text()).toEqual('Cancel Update');
+        });
+        
+        it("shows editable fields when startEditing is called", function() {
+            parentScope.theShop = {'name': 'Test editable shop to be',
+                                   'owner': '/api/v1/auth/user/1/',
+                                   "location": {"coordinates": [6.8, 1.3], "type": "Point",
+                                                "lat": 1.3, "long": 6.8},
+                                   'beingEdited': false,
+                                   'editable': true};
+            $compile(elm)(parentScope);
+            parentScope.$digest();
+            dirScope = elm.isolateScope();
+            dirScope.startEditing();
+            expect(dirScope.shop.beingEdited).toEqual(true);
+            parentScope.$digest();
+            expect(elm.find('div input').first().val()).toEqual('Test editable shop to be');
+            expect(elm.find('div div input').first().val()).toEqual('1.3');
+            expect(elm.find('div div input').last().val()).toEqual('6.8');
+            expect(elm.find('div button').first().text()).toEqual('Save Update');
+            expect(elm.find('div button').last().text()).toEqual('Cancel Update');
+        });
+        
+        it("hides editable fields when cancelEdits is called", function() {
+            parentScope.theShop = {'name': 'Test shop',
+                                   'owner': '/api/v1/auth/user/1/',
+                                   "location": {"coordinates": [6.8, 1.3], "type": "Point",
+                                                "lat": 1.3, "long": 6.8},
+                                   'beingEdited': true,
+                                   'editable': true};
+            $compile(elm)(parentScope);
+            parentScope.$digest();
+            dirScope = elm.isolateScope();
+            dirScope.cancelEdits();
+            expect(dirScope.shop.beingEdited).toEqual(false);
+            parentScope.$digest();
+            expect(elm.find('div div').first().text()).toEqual('Test shop');
+            expect(elm.find('div div div').first().text()).toEqual('1.300000');
+            expect(elm.find('div div div').last().text()).toEqual('6.800000');
+            expect(elm.find('div button').text()).toEqual('Update');
+            expect(dirScope.shop).toBeDefined();
+            expect(dirScope.shop.name).toEqual('Test shop');
+        });
+        
+        it("invokes update and stops editing when saveEdits is called", function() {
+            parentScope.theShop = {'id': 3,
+                                   'name': 'Test shop',
+                                   'owner': '/api/v1/auth/user/1/',
+                                   "location": {"coordinates": [6.8, 1.3], "type": "Point",
+                                                "lat": 1.11, "long": 7.77},
+                                   'beingEdited': true,
+                                   'editable': true};
+            $compile(elm)(parentScope);
+            parentScope.$digest();
+            dirScope = elm.isolateScope();
+            dirScope.saveEdits();
+            
+            expect(dirScope.shop.location.coordinates[0]).toEqual(7.77);
+            expect(dirScope.shop.location.coordinates[1]).toEqual(1.11);
+            parentScope.$digest();
+            expect(dirScope.shop.beingEdited).toEqual(false);
+            expect(elm.find('div div').first().text()).toEqual('Test shop');
+            expect(elm.find('div div div').first().text()).toEqual('1.110000');
+            expect(elm.find('div div div').last().text()).toEqual('7.770000');
+            expect(elm.find('div button').text()).toEqual('Update');
         });
     });
     
@@ -103,6 +172,11 @@ describe("tests for the cigar shops directives", function() {
             expect(elm.find('span').last().text()).toEqual('Here are your existing cigar shops:');
             expect(elm.find('div div button').last().text()).toEqual('Delete');
             expect(elm.find('dt-cigar-shop').size()).toEqual(2);
+            expect(elm.find('dt-cigar-shop').last().find('button').text()).toEqual('Update');
+            expect(dirScope.shops.a.beingEdited).toEqual(false);
+            expect(dirScope.shops.a.editable).toEqual(true);
+            expect(dirScope.shops.a.location.lat).toEqual(37.067922);
+            expect(dirScope.shops.a.location.long).toEqual(-75.130205);
         });
         
         it("invokes the saveShop method and updates on success", function() {
@@ -121,6 +195,11 @@ describe("tests for the cigar shops directives", function() {
             expect(elm.find('dt-cigar-shop').size()).toEqual(2);
             expect(elm.find('dt-cigar-shop').last().find('div div').first().text()).toEqual('freshly saved shop');
             expect(dirScope.shopToSave.name).toEqual('');
+            expect(dirScope.shops.testid.name).toEqual('freshly saved shop');
+            expect(dirScope.shops.testid.beingEdited).toEqual(false);
+            expect(dirScope.shops.testid.editable).toEqual(true);
+            expect(dirScope.shops.testid.location.lat).toEqual(37.222222);
+            expect(dirScope.shops.testid.location.long).toEqual(-75.111111);
         });
         
         it("invokes the removeShop method and updates on success", function() {
--- template_app/static/js/template_app/cigarshops/directives.js
+++ template_app/static/js/template_app/cigarshops/directives.js
@@ -1,13 +1,27 @@
-angular.module('template_app').directive('dtCigarShop', [function() {
+angular.module('template_app').directive('dtCigarShop', ['CigarShop', function(CigarShop) {
     "use strict";
     return {
         restrict: 'E',
         templateUrl: '/static/html/template_app/cigarshops/cigar_shop.html',
         scope: {
             shop: '=',
-            editable: '='
         },
         link: function (scope, element, attrs) {
+            scope.startEditing = function() {
+                scope.shop.beingEdited = true;
+            };
+            
+            scope.cancelEdits = function() {
+                scope.shop.beingEdited = false;
+            };
+            
+            scope.saveEdits = function() {
+                scope.shop.location.coordinates = [parseFloat(scope.shop.location.long),
+                                                   parseFloat(scope.shop.location.lat)];
+                CigarShop.update({id: scope.shop.id}, scope.shop).$promise.then(function() {
+                    scope.shop.beingEdited = false;
+                });
+            };
         },
     };
 }]);
@@ -26,17 +40,26 @@ angular.module('template_app').directive('dtCigarShops', ['CigarShop', 'saveAndG
             scope.shops = {};
             scope.shopToSave = {'name': '',
                                 'location': {"lat": 0, "long": 0, "type": "Point"},
-                                'owner': scope.currentuser.the_obj.resource_uri};
+                                'owner': scope.currentuser.the_obj.resource_uri,
+                                'beingEdited': true,
+                                'editable': false};
             scope.saveShop = function() {
                 scope.shopToSave.location.coordinates = [parseFloat(scope.shopToSave.location.long),
                                                          parseFloat(scope.shopToSave.location.lat)];
                 delete scope.shopToSave.location.lat;
                 delete scope.shopToSave.location.long;
                 saveAndGet(CigarShop.save, scope.shopToSave).then(function(newShop) {
-                    scope.shops[newShop.data.id] = newShop.data;
+                    var actualShop = newShop.data;
+                    actualShop.beingEdited = false;
+                    actualShop.editable = true;
+                    actualShop.location.lat = actualShop.location.coordinates[1];
+                    actualShop.location.long = actualShop.location.coordinates[0];
+                    scope.shops[actualShop.id] = actualShop;
                     scope.shopToSave = {'name': '',
                                         'location': {"lat": 0, "long": 0, "type": "Point"},
-                                        'owner': scope.currentuser.the_obj.resource_uri};
+                                        'owner': scope.currentuser.the_obj.resource_uri,
+                                        'beingEdited': true,
+                                        'editable': false};
                 });
             };
             scope.removeShop = function(shopId) {
@@ -46,6 +69,10 @@ angular.module('template_app').directive('dtCigarShops', ['CigarShop', 'saveAndG
             };
             CigarShop.get().$promise.then(function(data) {
                 data.objects.forEach(function(curShop) {
+                    curShop.beingEdited = false;
+                    curShop.editable = true;
+                    curShop.location.lat = curShop.location.coordinates[1];
+                    curShop.location.long = curShop.location.coordinates[0];
                     scope.shops[curShop.id] = curShop;
                 });
             });
