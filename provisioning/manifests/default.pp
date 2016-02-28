213f2a84e71be0fe994a1f208a34eb6400313cab
mcjug2015 <victor.semenov@gmail.com>
2016-02-28 02:59:19 -0500

    provisioning work started. very very far from done

---
+++ provisioning/manifests/default.pp
@@ -0,0 +1,23 @@
+$dt_user_home = "/home/dtuser/"
+
+class create_groups_users_dirs {
+    group { "dtuser":
+        ensure => present,
+        gid    => 1002
+    }
+
+    group { "dtowners":
+        ensure => present,
+        gid    => 1003
+    }
+
+    user { "dtuser":
+        ensure     => present,
+        gid        => "1002",
+        uid        => "1002",
+        membership => minimum,
+        require    => Group["dtowners"],
+        home       => $dt_user_home,
+        managehome => true,
+    }
+}
