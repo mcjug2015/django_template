a8295065d1da13450a02f2e054054ea9ebc98fa0
mcjug2015 <victor.semenov@gmail.com>
2016-02-29 22:00:36 -0500

    adding a dtsudo user to provisioning script

--- provisioning/environments/default/manifests/site.pp
+++ provisioning/environments/default/manifests/site.pp
@@ -1,6 +1,23 @@
 $dt_user_home = "/home/dtuser/"
+$dt_sudo_home = "/home/dtsudo/"
+
+
+class { 'sudo':
+    purge               => false,
+    config_file_replace => false,
+}
+
+
+class privileges {
+    sudo::conf { 'admins':
+      content => '%admins ALL=(ALL) NOPASSWD: ALL',
+    }
+}
+include privileges
+
 
 class create_groups_users_dirs {
+    
     group { "dtuser":
         ensure => present,
         gid    => 1002
@@ -11,14 +28,35 @@ class create_groups_users_dirs {
         gid    => 1003
     }
 
+    group { "dtsudo":
+        ensure => present,
+        gid    => 1004
+    }
+
+    group { "admins":
+        ensure => present,
+        gid    => 1005
+    }
+
     user { "dtuser":
         ensure     => present,
         gid        => "1002",
         uid        => "1002",
         membership => minimum,
-        require    => Group["dtowners"],
+        require    => [Group["dtowners"], Group["dtuser"]],
         groups     => ["dtowners"],
         home       => $dt_user_home,
+        managehome => true,
+    }
+    
+    user { "dtsudo":
+        ensure     => present,
+        gid        => "1004",
+        uid        => "1004",
+        membership => minimum,
+        require    => [Group["dtowners"], Group["dtsudo"], Group["admins"], Class["privileges"]],
+        groups     => ["dtowners", "admins"],
+        home       => $dt_sudo_home,
         managehome => true,
     }
 }
