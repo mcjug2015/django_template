3200f7b5a67d5adb6a5a9187b309ba3d5e3ef81a
Victor Semenov <victor.semenov@gmail.com>
2016-04-26 21:32:01 -0400

    adding bash script that installs puppet agent and modules

---
+++ provisioning/terraform/do_puppet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# install puppet agent and modules. this will be invoked by cloud init.
+/usr/bin/rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+/usr/bin/yum -y install puppet
+/usr/bin/puppet module install stahnma-epel
+/usr/bin/puppet module install m0byd1ck-repoforge
+/usr/bin/puppet module install puppetlabs-postgresql
+/usr/bin/puppet module install stankevich-python
+/usr/bin/puppet module install puppetlabs-vcsrepo
+/usr/bin/puppet module install jfryman-nginx
+/usr/bin/puppet module install saz-sudo
+/usr/bin/puppet module install jfryman-selinux
