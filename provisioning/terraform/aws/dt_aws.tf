c52bd89feb7459bc26b9498b509af0f50139f87d
Victor Semenov <victor.semenov@gmail.com>
2016-05-09 21:51:55 -0400

    broken aws tf that tries to use a custom vpc

--- provisioning/terraform/aws/dt_aws.tf
+++ provisioning/terraform/aws/dt_aws.tf
@@ -20,6 +20,16 @@ variable "local_ip" {
   default = "73.133.230.159/32"
 }
 
+variable "vpc_cidr" {
+  description = "The cidr to use for dt_vpc"
+  default = "172.32.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "The cidr to use for dt_subnet"
+  default = "172.32.0.0/20"
+}
+
 variable "pem_key_name" {
   description = "Name of the key for your aws .pem file"
   default = "victors_keypair"
@@ -32,9 +42,30 @@ provider "aws" {
     region = "${var.region_name}"
 }
 
+resource "aws_vpc" "dt_vpc" {
+    cidr_block = "${var.vpc_cidr}"
+    instance_tenancy = "default"
+    enable_dns_hostnames = true
+
+    tags {
+        Name = "dt_vpc"
+    }
+}
+
+resource "aws_subnet" "dt_subnet" {
+    vpc_id = "${aws_vpc.dt_vpc.id}"
+    cidr_block = "${var.subnet_cidr}"
+    map_public_ip_on_launch = true
+
+    tags {
+        Name = "dt_subnet"
+    }
+}
+
 resource "aws_security_group" "ssh_and_http" {
   name = "ssh_and_http"
   description = "Allows all outbound traffic, all http inbound, ssh inbound from defined ip"
+  vpc_id = "${aws_vpc.dt_vpc.id}"
 
   ingress {
       from_port = 80
@@ -68,7 +99,9 @@ resource "aws_security_group" "ssh_and_http" {
 resource "aws_instance" "dt_web" {
     ami = "ami-6d1c2007"
     instance_type = "t2.medium"
-    security_groups = ["ssh_and_http"]
+    subnet_id = "${aws_subnet.dt_subnet.id}"
+    vpc_security_group_ids = ["${aws_security_group.ssh_and_http.id}"]
+    associate_public_ip_address = true
     key_name = "${var.pem_key_name}"
     instance_initiated_shutdown_behavior = "terminate"
     root_block_device {
